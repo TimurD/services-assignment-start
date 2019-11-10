2ef976d872526ee673b497e42c547ff58a9d5dc6
Tymur Dadashov <Tymur_Dadashov@epam.com>
2019-11-11 01:17:34 +0200

    cleanup

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,4 +1,4 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {CounterService} from './counter-service.service';
 
 @Injectable()
@@ -7,11 +7,8 @@ export class UserService {
   constructor(private counterService: CounterService) {
   }
 
-  users: string[];
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
-  userSetToInactive = new EventEmitter<number>();
-  userSetToActive = new EventEmitter<number>();
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
